e9fdc2fec0ce52e7b60f2b296462fb7576966250
Tomáš Sabol <tomassabol2@gmail.com>
2026-01-08 22:39:22 +0100

    Patch Lambda powertools logger custom key

--- src/lambda/service-name.ts
+++ src/lambda/service-name.ts
@@ -16,5 +16,5 @@ export function getServiceName(): string {
 
 export function setServiceName(serviceName: string) {
   service = serviceName
-  logger.appendKeys({ service })
+  logger.appendKeys({ serviceName })
 }
